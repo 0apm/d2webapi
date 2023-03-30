4cff6b78c0ef88028c3255e40d37b028ec502984
0apm <0apm.tv@gmail.com>
2023-03-31 04:51:49 +0600

    structure changes

--- __init__.py
+++
@@ -1,3 +0,0 @@
-"""Top-level package for Dota 2 methods in Steam Web API."""
-
-from d2webapi import *
--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from d2webapi import Dota2API
+from src.d2webapi.d2webapi import Dota2API
 from config import STEAM_API_KEY
 
 def main():
@@ -6,6 +6,9 @@ def main():
     #print(api.get_match_detail(7081579203))
     #print(api.get_league_matches(15137))
     api.get_team_info_by_teamid(8722443)
+    api.get_team_info_by_teamid(8722443)
+
 
 if __name__ == "__main__":
    main()
+
